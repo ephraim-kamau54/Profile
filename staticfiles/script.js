304eeb26bfab72fdf17bfa33d34be2eaa5b86d4f
ephraim-kamau54 <ephraim.kamau@student.moringaschool.com>
2021-08-11 18:12:55 +0300

    re-edited script.js file

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -37,14 +37,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer"],
+        strings: ["Developer", "Blogger", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer"],
+        strings: ["Developer", "Blogger", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
